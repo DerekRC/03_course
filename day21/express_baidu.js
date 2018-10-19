e3e1c1187a4482244c4f58aa2dad73bc71f8b0aa
Derek <260@qq.com>
2018-10-19 17:34:59 +0800

    1.day22 All 2.yike-template项目

--- day21/express_baidu.js
+++ day21/express_baidu.js
@@ -6,7 +6,7 @@ app.listen(4000);
 
 app.get('/',function(req,res){
   // res.send('测试数据');
-  request('http://www.baidu.com',function(err,response,body){
+  request('https://moment.douban.com/api/stream/date/2017-5-11?alt=json&apikey=0bcf52793711959c236df76ba534c0d4&app_version=1.7.4&douban_udid=d623045db9fcb0d5243174c1bf1a675f887047c0&format=full&udid=9a34d8b038ff38971050199b0c5ee9c60c6d1ca3&version=6',function(err,response,body){
     res.send(body);
   })
 });
